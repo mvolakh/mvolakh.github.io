a86e105b061995c70ef9070563521e53311db62a
Mikita Volakh <m.volakh@gmail.com>
2023-04-03 23:10:32 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,22 @@ import App from './App.vue'
 import router from './router'
 
 import 'vuetify/styles'
+import "@mdi/font/css/materialdesignicons.css";
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 const vuetify = createVuetify({
     components,
     directives,
+    icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {
+        mdi,
+      }
+    }
   })
 
 createApp(App).use(vuetify).use(router).mount('#app')
