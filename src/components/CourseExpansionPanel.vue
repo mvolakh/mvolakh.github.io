c521be1312f631d08f84dac7c6d90d169b06e5b4
Mikita Volakh <m.volakh@gmail.com>
2023-04-04 03:06:28 +0200

    CourseExpansionPanel.vue: expansion panel component for each course

---
+++ src/components/CourseExpansionPanel.vue
@@ -0,0 +1,55 @@
+<script lang="ts">
+import { Prop } from 'vue';
+import { defineComponent, PropType } from 'vue'
+
+import CourseType from '../types/CourseType'
+
+export default defineComponent({
+    name: 'CourseExpansionPanel',
+    props: {
+      courses: {
+        required: true,
+        type: Array as PropType<CourseType[]>
+      }
+    },
+    components: {}
+})
+</script>
+
+<template>
+    <v-expansion-panels variant="accordion">
+      <v-expansion-panel v-for="course in courses" :title="course.name">
+        <v-expansion-panel-text>
+          <v-row class="text-center">
+            <v-col cols="12" md="4" class="d-flex justify-center flex-column">
+              <div>
+                <v-rating v-model="course.difficulty" size="x-small" readonly></v-rating>
+                <h5>Difficulty</h5>
+              </div>
+            </v-col>
+            <v-col cols="12" md="4" class="d-flex justify-center flex-column">
+              <div>
+                <v-rating v-model="course.enjoyability" size="x-small" readonly></v-rating>
+                <h5>Enjoyability</h5>
+              </div>
+            </v-col>
+            <v-col cols="12" md="4" class="d-flex justify-center flex-column">
+              <div>
+                <v-rating v-model="course.practicality" size="x-small" readonly></v-rating>
+                <h5>Practicality</h5>
+              </div>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              <p>{{ course.desc }}</p>
+            </v-col>
+          </v-row>
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </template>
+  
+<style scoped>
+</style>
+  
