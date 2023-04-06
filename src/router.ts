c25406f693ca46c8365fbd61d03eb57356e91b47
Mikita Volakh <m.volakh@gmail.com>
2023-04-06 04:27:05 +0200

    routes not found after refresh fix

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from './views/Home.vue'
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
