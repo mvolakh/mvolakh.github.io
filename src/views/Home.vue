7099ce90f9fae8dc4479dd161d93582effa240b0
Mikita Volakh <m.volakh@gmail.com>
2024-05-21 18:36:24 +0200

    fix: component import fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,4 @@
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-    name: 'Home',
-    components: {},
-    props: {},
-    setup() {
-        return {}
-    }
-})
+<script setup lang="ts">
 </script>
 
 <template>
@@ -16,7 +6,7 @@ export default defineComponent({
     <v-row>
     <v-col class="d-flex flex-column justify-center align-center">
         <h1>¯\_(ツ)_/¯</h1>
-        <p>New version coming soon</p>
+        <p>New version is coming soon</p>
         <p>27.05</p>
     </v-col>
     </v-row>
